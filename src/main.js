618cbff504f0621738d07764b5165166cf105ce4
Ruslan_Kurtjanyk <ruslan.kurtjanyk@gmail.com>
2017-11-26 22:33:58 +0200

    clear unneed content, install firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+import VueFire from 'vuefire'
+
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyAnhUVK03j3ho9PyUfo71p3YLFJ4lzZPOQ",
+    authDomain: "votes-io.firebaseapp.com",
+    databaseURL: "https://votes-io.firebaseio.com",
+    projectId: "votes-io",
+    storageBucket: "votes-io.appspot.com",
+    messagingSenderId: "664945492"
+};
+
+let firebaseApp = firebase.initializeApp(config)
+let db = firebaseApp.database()
 
 Vue.config.productionTip = false
 
